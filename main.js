2ae85b21d7bc1f52a56a5bb8331fb1e74e8c034c
solacite <julia.nguyen.do@gmail.com>
2025-10-19 09:49:00 -0400

    fix main + package

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 const { app, BrowserWindow } = require('electron') // import electron modules
 
-const createWindow = () => {
+const createWindow = () => { // loads web page into new BrowserWindow instance
     const win = new BrowserWindow({
         width: 800,
         height: 600
@@ -9,6 +9,18 @@ const createWindow = () => {
     win.loadFile('index.html')
 }
 
-app.whenReady().then(() => {
+app.whenReady().then(() => { // calls function when app is ready
     createWindow()
+
+    app.on('activate', () => { // re-creates window
+        if (BrowserWindow.getAllWindows().length === 0) {
+            createWindow()
+        }
+    })
+})
+
+app.on('window-all-closed', () => { // quits app when all windows are closed
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
